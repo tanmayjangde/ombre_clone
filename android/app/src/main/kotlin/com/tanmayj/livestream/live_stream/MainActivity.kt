b82e3ff0460ce1e48e280a08e6476b0c8726d45f
tanmayjangde <tanmayjangde7@gmail.com>
2022-10-02 21:46:21 +0530

    Login Done

---
+++ android/app/src/main/kotlin/com/tanmayj/livestream/live_stream/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tanmayj.livestream.live_stream
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'view/screens/sign_in.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterFire Samples',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
